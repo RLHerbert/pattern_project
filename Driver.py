9b89953516a99d43b234ce79e1d7ea188c64a0ea
Dennis La <dennisla95@yahoo.com>
2020-10-18 09:32:18 -0700

    create driver and dtree methods class

---
+++ Driver.py
@@ -0,0 +1,4 @@
+from DtreeMethods import DtreeMethods
+
+data = []
+DtreeMethods.find_best_attribute(data)
---
+++ DtreeMethods.py
@@ -0,0 +1,5 @@
+class DtreeMethods:
+
+    @staticmethod
+    def find_best_attribute(dataset):
+        pass
