034fcf4bd77e5a544eea780b910c20aa75d63b9c
Rowan Herbert <rlherbert.dev@gmail.com>
2020-10-22 18:26:39 -0700

    Fixed off by 1 error

--- parse.py
+++ parse.py
@@ -2,41 +2,44 @@ import csv
 import random
 
 
-def data_to_array(row=[]):
-    return [row[0], row[1], row[2], row[3], row[4], row[5], row[6]]
+# Takes the enumeration of the row, a tuple, and places the id inside the list for convenience
+def give_ids(enumerated=[]):
+    row_with_id = enumerated[1]
+    row_with_id.insert(0, str(enumerated[0]))
+    return row_with_id
 
 
-def enumerate_data(enumerated=[]):
-    temp = enumerated[1]
-    temp.insert(0, str(enumerated[0]))
-    return temp
-
-
-def parse():  # Opens the CSV file and parses it into a list of lists
+# Opens the CSV file and parses it into a list of lists
+def parse():
     with open('data_set.csv', newline='\n') as data_set:
         # Creates list of lists from CSV
         data_reader = csv.reader(data_set, delimiter=',')
         # Assigns an ID to each vector then shuffles it
-        shuffled = list(map(enumerate_data, list(enumerate(data_reader))))
+        shuffled = list(map(give_ids, list(enumerate(data_reader))))
         random.shuffle(shuffled)
         return shuffled
 
 
-def run():  # Splits our data set into a training set, holdout set, and validation set
+# Splits our data set into a training set, holdout set, and validation set
+def run():
     shuffled_complete_data_set = parse()
     split_data_sets = {
-        "train": shuffled_complete_data_set[:131],      # Training data set
-        "holdt": shuffled_complete_data_set[132:175],   # Holdout data set
-        "valid": shuffled_complete_data_set[176:]       # Validation data set
+        # Training data set
+        "train": shuffled_complete_data_set[0:132],
+        # Holdout data set
+        "holdt": shuffled_complete_data_set[132:176],
+        # Validation data set
+        "valid": shuffled_complete_data_set[176:220]
     }
     return split_data_sets
 
 
 def main():
-    print(run())
-    # data_set = parse()
-    # for row in data_set:
-    #     print(row)
+    data = run()
+    # print(data)
+    print('Size of training set is 132: ' + str(len(data['train']) == 132))
+    print('Size of holdout set is 44: ' + str(len(data['holdt']) == 44))
+    print('Size of validation set is 44: ' + str(len(data['valid']) == 44))
 
 
 if __name__ == "__main__":
